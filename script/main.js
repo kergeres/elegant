cf54549addbb2ef8e976e40d3edeed20d46869d3
kergeres <55786735+kergeres@users.noreply.github.com>
2021-02-04 22:56:30 +0100

    mobile version pipa-meg szinek kellenek meg aprosagok

--- script/main.js
+++ script/main.js
@@ -1,5 +1,52 @@
 "use strict";
 
+// hamburger navigacio 
+let cbOpen = true;
+function hamburgerNav() {
+ 
+    if(cbOpen==false)//ha zarva van
+    {
+        document.querySelector(".nnav").classList.remove("nav-open");
+        document.querySelector(".ham-menu-line").classList.remove("line-rot");
+        document.querySelector(".ham-menu-linea").classList.remove("linea-rot");
+        document.querySelector(".ham-menu-lineb").classList.remove("lineb-rot");
+        
+        cbOpen = true;
+    }
+    else if(cbOpen==true)
+    {
+      
+        document.querySelector(".nnav").classList.add("nav-open");
+        document.querySelector(".ham-menu-line").classList.add("line-rot");
+        document.querySelector(".ham-menu-linea").classList.add("linea-rot");
+        document.querySelector(".ham-menu-lineb").classList.add("lineb-rot");
+
+
+        document.querySelector(".nnav").addEventListener('click', function() {
+          document.querySelector(".nnav").classList.remove("nav-open");
+          document.querySelector(".ham-menu-line").classList.remove("line-rot");
+          document.querySelector(".ham-menu-linea").classList.remove("linea-rot");
+          document.querySelector(".ham-menu-lineb").classList.remove("lineb-rot");
+          cbOpen = true;
+        });
+        cbOpen = false;
+    }  
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 async function loadData() {
   let response = await fetch("/data/json.json");
   let jsonData = await response.json();
